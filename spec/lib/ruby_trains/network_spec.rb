5ae50bd3fe159bcd8cee4209ef864b610feeeedd
Joe Sustaric <joe8307@gmail.com>
2016-09-10 11:09:30 +1000

    adding a more complicated example for the network generation

--- spec/lib/ruby_trains/network_spec.rb
+++ spec/lib/ruby_trains/network_spec.rb
@@ -7,7 +7,7 @@ describe RubyTrains::Network do
 
     context 'Given no network input' do
 
-      context 'When we create a new network' do
+      context 'When we create the new network' do
         subject { RubyTrains::Network.new }
 
         it 'creates an empty network' do
@@ -19,7 +19,7 @@ describe RubyTrains::Network do
     context 'Given a single input (2 stations 1 connection)' do
       let(:input) { 'AB1' }
 
-      context 'When we create a new network' do
+      context 'When we create the new network' do
         subject { RubyTrains::Network.new input }
 
         it 'creates the two stations' do
@@ -41,7 +41,7 @@ describe RubyTrains::Network do
     context 'Given 2 stations with 1 connection each' do
       let(:input) { 'AB1 BC2' }
 
-      context 'When we create a network' do
+      context 'When we create the network' do
         subject { RubyTrains::Network.new input }
 
         it 'creates all the correct stations' do
@@ -69,7 +69,7 @@ describe RubyTrains::Network do
     context 'Given a station with multiple connections' do
       let(:input) { 'AB1 BC2 AC3' }
 
-      context 'When we create a network' do
+      context 'When we create th network' do
         subject { RubyTrains::Network.new input }
 
         it 'creates all the correct stations' do
@@ -92,6 +92,45 @@ describe RubyTrains::Network do
         end
 
       end
+    end
+
+    context 'Given more stations with more connections (complex)' do
+      let(:input) { 'AB5 BC4 CD8 DC8 DE6 AD5 CE2 EB3 AE7' }
+
+      context 'When we create the network' do
+        subject { RubyTrains::Network.new input }
+
+        it 'creates all the correct stations' do
+          expect(subject.stations.size).to eq 5
+        end
+
+        it 'adds the correct connections from station A' do
+          a_connections = subject.stations['A'].connections
+          expect(a_connections.size).to eq 3
+        end
+
+        it 'adds the correct connection to station B' do
+          b_connections = subject.stations['B'].connections
+          expect(b_connections.size).to eq 1
+        end
+
+        it 'adds no connections from C' do
+          c_connections = subject.stations['C'].connections
+          expect(c_connections.size).to eq 2
+        end
+
+        it 'adds the correct connection to station D' do
+          d_connections = subject.stations['D'].connections
+          expect(d_connections.size).to eq 2
+        end
+
+        it 'adds no connections from C' do
+          e_connections = subject.stations['E'].connections
+          expect(e_connections.size).to eq 1
+        end
+
+      end
+
     end
   end
 
