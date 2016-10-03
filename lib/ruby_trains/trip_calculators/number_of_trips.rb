ad5e4a10260767c222f4705d5a196a11891d9118
Joe Sustaric <joe8307@gmail.com>
2016-10-03 13:15:50 +1100

    removing validation for trips from the logic class

--- lib/ruby_trains/trip_calculators/number_of_trips.rb
+++ lib/ruby_trains/trip_calculators/number_of_trips.rb
@@ -7,12 +7,9 @@ module RubyTrains
     # When false it will be < or = max_stops.
     class NumberOfTrips
       ERROR = -1
-      # Matches A-B , AA-B, Foo-Bar
-      TRIP_REGEX = /^([A-Za-z]+)-([A-Za-z]+)$/
       INITIAL_TRAVELLED_DISTANCE = 0
 
       def self.calculate(network, trip, max_stops, exact)
-        return ERROR unless trip_input_valid?(trip, max_stops)
         calc_vars = initialize_calulation_vars(network, trip, max_stops, exact)
         get_trips(calc_vars, INITIAL_TRAVELLED_DISTANCE)
         calc_vars[:number_of_routes]
@@ -34,7 +31,6 @@ module RubyTrains
       end
 
       def self.initialize_calulation_vars(network, trip, max_stops, exact)
-        trip = trip.split('-')
         {
           from_station: network.stations[trip[0]],
           to_station: network.stations[trip[1]],
@@ -50,13 +46,9 @@ module RubyTrains
         (calc_vars[:to_station] == connection.station) && stop_limit
       end
 
-      def self.trip_input_valid?(trip, max_stops)
-        !trip.empty? && (trip =~ TRIP_REGEX) && max_stops.positive?
-      end
-
       public_class_method :calculate
 
-      private_class_method :trip_input_valid?, :at_dest_and_in_stop_limit?
+      private_class_method :at_dest_and_in_stop_limit?
       private_class_method :initialize_calulation_vars, :get_trips
     end
   end
--- spec/lib/ruby_trains/trip_calculators/number_of_trips_spec.rb
+++ spec/lib/ruby_trains/trip_calculators/number_of_trips_spec.rb
@@ -12,7 +12,7 @@ module RubyTrains
 
           context 'When we calculate number of trips between 2 different stations'\
                   ' with a max stops <= limit' do
-            let(:trip) { 'A-C' }
+            let(:trip) { %w(A C) }
             let(:max_stops) { 2 }
             let(:expected) { 1 }
 
@@ -22,24 +22,13 @@ module RubyTrains
             end
 
           end
-
-          context 'When we try to calculate a garbage trip' do
-            let(:trip) { 'FooBar3095uijreglkn' }
-            let(:max_stops) { 2 }
-            let(:expected) { -1 }
-
-            it 'returns ERROR' do
-              result = NumberOfTrips.calculate(network, trip, max_stops, false)
-              expect(result).to eq expected
-            end
-          end
         end
 
         context 'Given a simple network with multiple routes' do
           let(:network) { Network.new %w(AB1 BC2 AD4 DC3) }
 
           context 'When we calculate 2 diff stations & max stops <= limit' do
-            let(:trip) { 'A-C' }
+            let(:trip) { %w(A C) }
             let(:max_stops) { 2 }
             let(:expected) { 2 }
 
@@ -57,7 +46,7 @@ module RubyTrains
 
           context 'When we ask for number of trips between the same station'\
                   ' with a max stops <= limit' do
-            let(:trip) { 'C-C' }
+            let(:trip) { %w(C C) }
             let(:max_stops) { 3 }
             let(:expected) { 2 }
 
@@ -69,7 +58,7 @@ module RubyTrains
 
           context 'When we ask for number of trips between different stations'\
                   ' with a max stops == limit' do
-            let(:trip) { 'A-C' }
+            let(:trip) { %w(A C) }
             let(:max_stops) { 4 }
             let(:expected) { 3 }
 
