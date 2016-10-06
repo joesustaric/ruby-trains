4877e4ce8564b79bfbbd764439c1cd9064fffcfa
Joe Sustaric <joe8307@gmail.com>
2016-10-06 22:09:31 +1100

    refactoring to ensure all vars are ivars

--- lib/ruby_trains/trip_calculators/number_of_routes_distance.rb
+++ lib/ruby_trains/trip_calculators/number_of_routes_distance.rb
@@ -4,45 +4,45 @@ module RubyTrains
     # This will calculate how many paths there are between two stations,
     # that have a distance < the max_distance.
     class NumberOfRoutesDistance
+      STARTING_PATH_NUMBER = 0
+      A_PATH = 1
+
       def initialize(network)
         @network = network
+        @result = 0
       end
 
       def calculate(trip, max_distance)
-        calc_vars = initialize_calulation_vars(trip, max_distance)
-        how_many_paths(calc_vars[:start], calc_vars, 0)
-        calc_vars[:result]
+        set_ivars(trip, max_distance)
+        how_many_paths(@start_station, STARTING_PATH_NUMBER)
+        @result
       end
 
-      def how_many_paths(current, calc_vars, current_dist)
+      def how_many_paths(current, current_dist)
         current.connections.each do |_, c|
-          next unless dist_less_than_max?(calc_vars, current_dist + c.distance)
-          add_result_if_reached_destination calc_vars, c
-          how_many_paths(c.station, calc_vars, (current_dist + c.distance))
+          next unless dist_less_than_max?(current_dist + c.distance)
+          add_result_if_reached_destination c
+          how_many_paths(c.station, (current_dist + c.distance))
         end
       end
 
-      def add_result_if_reached_destination(calc_vars, connection)
-        calc_vars[:result] += 1 if connection.station == calc_vars[:finish]
+      def add_result_if_reached_destination(connection)
+        @result += A_PATH if connection.station == @finish_station
       end
 
-      def initialize_calulation_vars(trip, max_distance)
-        {
-          start: @network.stations[trip[0]],
-          finish: @network.stations[trip[1]],
-          max_distance: max_distance,
-          result: 0
-        }
+      def set_ivars(trip, max_distance)
+        @start_station = @network.stations[trip[0]]
+        @finish_station = @network.stations[trip[1]]
+        @max_distance = max_distance
       end
 
-      def dist_less_than_max?(calc_vars, distance)
-        distance < calc_vars[:max_distance]
+      def dist_less_than_max?(distance)
+        distance < @max_distance
       end
 
       public :calculate
 
-      private :initialize_calulation_vars, :how_many_paths
-      private :dist_less_than_max?
+      private :set_ivars, :how_many_paths, :dist_less_than_max?
     end
   end
 end
