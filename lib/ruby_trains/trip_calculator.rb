04587acb3ff809f498d94a462408920054abeff3
joe Sustaric <joe8307@gmail.com>
2016-10-11 13:58:03 +1100

    adding in the first version of the trip calculator route distance calc

--- lib/ruby_trains/trip_calculator.rb
+++ lib/ruby_trains/trip_calculator.rb
@@ -3,13 +3,15 @@ require 'ruby_trains/trip_calculators/route_distance'
 require 'ruby_trains/trip_calculators/number_of_trips'
 require 'ruby_trains/trip_calculators/number_of_routes_distance'
 
-# trip calculator
-
-# static methods
-# mock calculator objects
-# it validates and
-
 module RubyTrains
+  # This class deals with parsing the input then doing the calculation with
+  # the relative trip calculator.
   class TripCalculator
+    def self.execute_route_dist_calc(network, trip)
+      n = Validator.parse_network network
+      t = Validator.parse_trip trip
+      calc = TripCalculators::ShortestRoute.new n
+      calc.calculate t
+    end
   end
 end
--- spec/lib/ruby_trains/trip_calculator_spec.rb
+++ spec/lib/ruby_trains/trip_calculator_spec.rb
@@ -1,8 +1,53 @@
 require 'spec_helper'
 require 'ruby_trains/trip_calculator'
+require 'ruby_trains/trip_calculators/shortest_route'
 
 module RubyTrains
+  # This test class exists only because I want to revise Rspec mocking
+  # Idealy I would just keep these as high level tests to ensure happy/sad paths
   describe TripCalculator do
+    describe '#execute_route_dist_calc' do
+      context 'Given valid input and no calculation error' do
+        let(:parsed_network) { %w(AB1) }
+        let(:parsed_trip) { %w(A B) }
+        let(:test_network) { 'AB1' }
+        let(:test_trip) { 'A-B' }
+        let(:network) { TripCalculators::ShortestRoute.new parsed_trip }
 
+        before do
+          allow(Validator).to receive(:parse_network).and_return parsed_network
+          allow(Validator).to receive(:parse_trip).and_return parsed_trip
+          allow(TripCalculators::ShortestRoute).to receive(:new).and_return network
+          allow(network).to receive(:calculate).and_return 99
+        end
+
+        it 'parses the input' do
+          expect(Validator).to receive(:parse_network).with test_network
+          expect(Validator).to receive(:parse_trip).with test_trip
+          expect(network).to receive(:calculate).with parsed_trip
+          result = TripCalculator.execute_route_dist_calc test_network, test_trip
+          expect(result).to eq 99
+        end
+
+        it 'calculates the route distance' do
+        end
+      end
+
+      context 'Given invalid network input' do
+
+        it 'raises an input error' do
+        end
+      end
+      context 'Given invalid trip input' do
+
+        it 'raises an input error' do
+        end
+      end
+      context 'Given a calculation error' do
+
+        it 'raises the calc error' do
+        end
+      end
+    end
   end
 end
