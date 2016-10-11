8f07a2f7f9fc354d31d6501ed8958ad4c13b8d0a
Joe Sustaric <joe8307@gmail.com>
2016-10-11 22:45:19 +1100

    adding in stupid mock tests

--- spec/lib/ruby_trains/trip_calculator_spec.rb
+++ spec/lib/ruby_trains/trip_calculator_spec.rb
@@ -3,14 +3,14 @@ require 'ruby_trains/trip_calculator'
 require 'ruby_trains/trip_calculators/shortest_route'
 
 module RubyTrains
-  # This test class exists only because I want to revise Rspec mocking
+  # This test class exists only because I want to revise Rspec mocking / stubs
   # Idealy I would just keep these as high level tests to ensure happy/sad paths
   describe TripCalculator do
     describe '#execute_route_dist_calc' do
       context 'Given valid input and no calculation error' do
-        let(:parsed_network) { %w(AB1) }
+        let(:test_network) { 'AB1 BC2' }
+        let(:parsed_network) { %w(AB1 BC2) }
         let(:parsed_trip) { %w(A B) }
-        let(:test_network) { 'AB1' }
         let(:test_trip) { 'A-B' }
         let(:network) { TripCalculators::ShortestRoute.new parsed_trip }
 
@@ -24,28 +24,46 @@ module RubyTrains
         it 'parses the input' do
           expect(Validator).to receive(:parse_network).with test_network
           expect(Validator).to receive(:parse_trip).with test_trip
-          expect(network).to receive(:calculate).with parsed_trip
-          result = TripCalculator.execute_route_dist_calc test_network, test_trip
-          expect(result).to eq 99
+          TripCalculator.execute_route_dist_calc test_network, test_trip
         end
 
-        it 'calculates the route distance' do
+        it 'calculates and returns a result' do
+          # don't care what the result is in this test
+          expect(network).to receive(:calculate).with parsed_trip
+          result = TripCalculator.execute_route_dist_calc test_network, test_trip
+          expect(result).to be_truthy
         end
       end
 
-      context 'Given invalid network input' do
+      context 'Given a network parsing error' do
+        before { allow(Validator).to receive(:parse_network).and_raise('boom') }
 
-        it 'raises an input error' do
+        it 'raises the input error' do
+          expect { TripCalculator.execute_route_dist_calc('foobar', 'A-B') }
+            .to raise_error('boom')
         end
       end
       context 'Given invalid trip input' do
+        before { allow(Validator).to receive(:parse_trip).and_raise('Pow!') }
 
-        it 'raises an input error' do
+        it 'raises the input error' do
+          expect { TripCalculator.execute_route_dist_calc('AB1', 'fooBar') }
+            .to raise_error('Pow!')
         end
       end
       context 'Given a calculation error' do
+        let(:n) { TripCalculators::ShortestRoute.new %w(AB1 BC2) }
+
+        before do
+          allow(Validator).to receive(:parse_network).and_return %w(AB1 BC2)
+          allow(Validator).to receive(:parse_trip).and_return %w(A B)
+          allow(TripCalculators::ShortestRoute).to receive(:new).and_return n
+          allow(n).to receive(:calculate).and_raise('Zomgs!')
+        end
 
         it 'raises the calc error' do
+          expect { TripCalculator.execute_route_dist_calc('balh', 'fooBar') }
+            .to raise_error('Zomgs!')
         end
       end
     end
